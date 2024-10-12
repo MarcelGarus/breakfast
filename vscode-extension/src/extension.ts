d2b99d25125f9afb1e5d84b8d45c7bf071449e61
Marcel Garus <github@marcelgarus.dev>
2024-10-12 20:55:21 +0200

    Add VSCode extension

---
+++ vscode-extension/src/extension.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+import * as vs from "vscode";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function activate(_: vs.ExtensionContext) {
+  console.info("Activated Breakfast extension!");
+}
+export function deactivate() {
+  // TODO: What to do here?
+}
